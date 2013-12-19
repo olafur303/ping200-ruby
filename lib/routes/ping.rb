8784d8da377b059ade0c161b516f09f09e9b5da3
Karl Litterfeldt <Kalle.Litterfeldt@gmail.com>
2013-12-19 14:41:58 +0100

    fixed routes

--- lib/routes/ping.rb
+++ lib/routes/ping.rb
@@ -5,13 +5,13 @@ module Ping200
       def self.registered(app)
         app.get '/ping/all' do
           objects = Controller::PingObjectController.all()
-          json events.map { |e| e.to_h }
+          json objects.map { |o| o.to_h }
         end
 
         app.get '/ping' do
           halt 500 unless params[:website]
           objects = Controller::PingObjectController.get params[:website]
-          json events.map { |e| e.to_h }
+          json objects.map { |o| o.to_h }
         end
       end
     end
--- lib/routes/site.rb
+++ lib/routes/site.rb
@@ -4,8 +4,8 @@ module Ping200
     module Site
       def self.registered(app)
         app.get '/site/all' do
-          sites = Controller::SitesController.get_all
-          json sites.to_h
+          sites = Controller::SitesController.all
+          json sites.map { |site| site.to_h }
         end
 
         app.post '/site/add' do
@@ -15,8 +15,8 @@ module Ping200
         end
 
         app.delete '/site/delete' do
-          halt 500 unless params[:website]
-          object = Controller::SitesController.add_site params[:website]
+          halt 500 unless params[:key]
+          object = Controller::SitesController.del params[:key]
         end
       end
     end
